1dd4192c581f1a2acdc9e3720642738d0feb5cbf
asamiul101 <58337911+samiularnob12@users.noreply.github.com>
2022-10-26 18:46:09 -0400

    css change

--- ASL-Learner/src/Register.js
+++ ASL-Learner/src/Register.js
@@ -1,3 +1,5 @@
+import React from "react";
+import './Register.css';
 import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
