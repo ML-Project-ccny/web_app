daaac6628e4b70962af7a7df9692cd431a804543
asamiul101 <58337911+samiularnob12@users.noreply.github.com>
2022-10-26 19:04:28 -0400

    changes

--- ASL-Learner/src/project.js
+++ ASL-Learner/src/project.js
@@ -1,15 +1,13 @@
 import React from "react";
 import './home.css';
 import './project.css';
-import {useRef, useEffect} from "react"
-
-
+import {useRef, useEffect, useState} from "react"
 
 
 function Project(){
 
 
-    var letters = ['A', 'B', 'C'];
+    //var letters = ['A', 'B', 'C'];
     
 
     const videoRef = useRef(null);
@@ -49,17 +47,61 @@ function Project(){
             console.log("not working")
         }
     }) */
+    
+    //const changeImage = (fileName) => {
+        //let img = document.querySelector("#bannerImage");
+        //img.setAtrribute(src, fileName);
+    //}
 
+   /*  const changeImage = () => {
+        let displayimg = document.getElementById('image1')
+        if (displayimg.src.match(pic1)) {
+            displayimg.src = pic2
+        } 
+        else if (displayimg.src.match(pic2)) {
+            displayimg.src = pic3
+        } 
+        else if (displayimg.src.match(pic3)) {
+            displayimg.src = pic4
+        } 
+        else{
+            displayimg.src = pic1
+        } 
+    } 
+ */
+
+    const pic1 = require("./images/a.jpg");
+    const pic2 = require("./images/p.jpg");
+    //const pic3 = require("./images/l.jpg");
+    //const pic4 = require("./images/e.jpg");
+
+    //const images = {pic1,pic2,pic3,pic4};
+    const[img, setImg] = useState(false);
+
+    const imgChangeHandler = () => {
+        if(!img) {
+            setImg(true);
+        }else{
+            setImg(false)
+        }
+    };
 
 
     return (
         <div>
             <div id="output">
-                <h1>
-                    {letters.map(function(name, index){
-                        return <h1><li key={ index }>{name}</li></h1>;
-                    })}
-                </h1>
+                {/* <img src={pic1} height="355px" weight="520px" alt="a" id="bannerImage"/> <br/>
+                <button onClick={<img src={pic1} height="355px" weight="520px" alt="a" id="bannerImage"/>}> first letter </button>
+                <button onClick={<img src={pic2} height="355px" weight="520px" alt="a" id="bannerImage"/>}> second letter </button>
+                <button onClick={<img src={pic2} height="355px" weight="520px" alt="a" id="bannerImage"/>}> third letter </button>
+                <button onClick={<img src={pic3} height="355px" weight="520px" alt="a" id="bannerImage"/>}> fourth letter </button>
+                <button onClick={<img src={pic4} height="355px" weight="520px" alt="a" id="bannerImage"/>}> fifth letter </button> */}
+                
+                {/* <img id="image1" src={pic1} alt="" onClick={changeImage}/>
+                <button onClick={changeImage} id="image1"> 1</button> */}
+
+                {<img src={!img ? pic1 : pic2} width="360px" height="680px" alt='pic1' onClick={imgChangeHandler}/>}
+
             </div> 
 
             <div id="camera">
@@ -67,6 +109,11 @@ function Project(){
                     <video id="videoElement" ref={videoRef}></video>
                 
             </div>
+
+            <div id="name">
+                <h1> Word: app</h1>
+            </div>
+
         </div>
                     
             
