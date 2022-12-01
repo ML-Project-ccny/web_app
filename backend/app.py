1b9f097f341abb952c6fda368b416205c3742bf8
mahmoudagag <agagmahmoud@gmail.com>
2022-11-30 20:24:59 -0500

    update

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,9 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import or_,and_
 import torchvision.transforms as tt
-
+import numpy as np
+from PIL import Image
+from collections import defaultdict
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
@@ -34,14 +36,15 @@ pytorch_model = ASLResnet()
 
 ## set params from the saved state of model
 pytorch_model.load_state_dict(torch.load('model.pt', map_location=torch.device('cpu')), strict=False)
-# pytorch_model.load_state_dict(torch.load('model.pt'), strict=False)
 
+# pytorch_model.load_state_dict(torch.load('model.pt'), strict=False)
 pytorch_model.eval()
 
 #template {level:[words...],...}
 APP_WORDS = {1:['Able','Love','Buy','Cube','Wavy','Bowl','Claw','You','Clay','Clue'],
 2:['Risk','Sir','Kids','Verb','Dark','Feud',"Four",'Cake'],
 3:['Foxy','Onyx','Gown','Honk','Minx','Hack','Claw','Hawk','Hack','Numb']}
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 @app.route('/',methods=['POST'])
 def index():
@@ -50,21 +53,30 @@ def index():
 
     arr = torch.FloatTensor(data['data'])
     height,width = data['height'], data['width']
+    print(data['letter'])
 
-    x = arr.reshape(4,height,width)
-    x = x[:3,:,:] 
-    # x = torch.div(x,255)
-    t = tt.ToPILImage()
-    img = t(x)
+    array = np.array(data['data'], np.uint8)
+    array = np.reshape(array, (height, width, 4))
+
+    new_image = Image.fromarray(array)
+    if data['hand'] == 'left':
+        new_image = new_image.transpose(method=Image.FLIP_LEFT_RIGHT)
     tfms = tt.Compose([tt.Resize((200, 200)),
                         tt.ToTensor()])
-    x = tfms(img)
-    x = x.reshape(1,3,200,200)
 
-    res = pytorch_model(x)
-    print(res)
-    print(torch.max(res,dim=1))
-    return "HELLO"
+    img_transform = tfms(new_image)
+    img_transform.unsqueeze_(0)
+
+    img_transform = img_transform[:,:3, :, :]
+    
+    res = pytorch_model(img_transform)
+
+    value = torch.max(res,dim=1)
+    number = value.values.tolist()
+    idx = value.indices.tolist()
+    print(ALPHABET[idx[0]])
+
+    return {"letter":ALPHABET[idx[0]],"value":number[0]}
 
 @app.route('/user',methods=['POST','GET'])
 def user():
@@ -97,19 +109,21 @@ def user():
             return "Login in"
         return "invalid infomation"
 
-@app.route('/word',methods=['GET','PATCH'])
+@app.route('/words',methods=['POST','PATCH'])
 def words():
     data = request.get_json(force=True)
     if not data['email'] :
         return "Not valid information"
     email = data['email']
     user = User.query.filter_by(email=email).first()
-    if request.method == 'GET': 
+    if request.method == 'POST': 
         # words =  db.session.execute(Words.query.filter_by(Words.user_id=user.id))
         words = db.session.query(Words).filter(Words.user_id == user.id)
-        res = []
+        print(words)
+        res = defaultdict(list)
         for r in words:
-            res.append({'level':r.level,'word':r.word,'score':r.score})
+            res[r.level].append((r.word,r.score))
+            # res.append({'level':r.level,'word':r.word,'score':r.score})
         return res
 
     if request.method == 'PATCH': 
@@ -122,17 +136,28 @@ def words():
         return 'score updated'
 @app.route('/allWords',methods=['GET'])
 def getAllWords():
-    return APP_WORDS
+    x = defaultdict(list)
+    for k,v in APP_WORDS.items():
+        for w in v:
+            x[k].append((w,0))
 
+    return x
+
+APP_WORDS = {1:['Able','Love','Buy','Cube','Wavy','Bowl','Claw','You','Clay','Clue'],
+2:['Risk','Sir','Kids','Verb','Dark','Feud',"Four",'Cake'],
+3:['Foxy','Onyx','Gown','Honk','Minx','Hack','Claw','Hawk','Hack','Numb']}
 
 def createWords(id):
-    word = Words(user_id=id,level=1,word='word')
-    try:
-        db.session.add(word)
-        db.session.commit()
-        return "user created"
-    except:
-        return "error occured"
+    #do it based on APP_WORDS
+    for k,v in APP_WORDS.items():
+        for w in v:
+            word = Words(user_id=id,level=k,word=w)
+            try:
+                db.session.add(word)
+            except:
+                continue
+    db.session.commit()
+    
 
 if __name__ == '__main__':
     with app.app_context():
